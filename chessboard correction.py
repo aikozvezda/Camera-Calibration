6add96173c6dcc30ab9139ce4d45840f26e65d3c
aikozvezda <144213771+aikozvezda@users.noreply.github.com>
2024-04-01 21:51:02 +0900

    Update chessboard correction.py

--- chessboard correction.py
+++ chessboard correction.py
@@ -2,15 +2,18 @@ import numpy as np
 import cv2 as cv
 
 # The given video and calibration data
-video_file = 'chessboard.avi'
-K = np.array([[432.7390364738057, 0, 476.0614994349778],
-              [0, 431.2395555913084, 288.7602152621297],
+video_file = '/chessboard.mp4'
+K = np.array([[350, 0, 350],
+              [0, 250, 250],
               [0, 0, 1]]) # Derived from `calibrate_camera.py`
 dist_coeff = np.array([-0.2852754904152874, 0.1016466459919075, -0.0004420196146339175, 0.0001149909868437517, -0.01803978785585194])
 
 # Open a video
 video = cv.VideoCapture(video_file)
 assert video.isOpened(), 'Cannot read the given input, ' + video_file
+
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'XVID')  # Codec 설정 (여기서는 MPEG-4 코덱 사용)
 
 # Run distortion correction
 show_rectify = True
@@ -30,6 +33,12 @@ while True:
         info = "Rectified"
     cv.putText(img, info, (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
 
+    # Define VideoWriter object inside the loop
+    out = cv.VideoWriter('output.mp4', fourcc, 20.0, (img.shape[1], img.shape[0]))
+
+    # Write the frame into the file
+    out.write(img)
+
     # Show the image and process the key event
     cv.imshow("Geometric Distortion Correction", img)
     key = cv.waitKey(10)
@@ -40,5 +49,7 @@ while True:
     elif key == ord('\t'):  # Tab: Toggle the mode
         show_rectify = not show_rectify
 
+# Release everything if job is finished
 video.release()
+out.release()
 cv.destroyAllWindows()
