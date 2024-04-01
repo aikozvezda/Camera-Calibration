13facdf61f3cf34fa02d891c867f5a88a90cb1ea
aikozvezda <144213771+aikozvezda@users.noreply.github.com>
2024-04-01 21:54:51 +0900

    chessboard correction.py

--- chessboard correction.py
+++ chessboard correction.py
@@ -1,30 +1,25 @@
 import numpy as np
 import cv2 as cv
 
-# The given video and calibration data
 video_file = '/chessboard.mp4'
 K = np.array([[350, 0, 350],
               [0, 250, 250],
-              [0, 0, 1]]) # Derived from `calibrate_camera.py`
+              [0, 0, 1]]) 
 dist_coeff = np.array([-0.2852754904152874, 0.1016466459919075, -0.0004420196146339175, 0.0001149909868437517, -0.01803978785585194])
 
-# Open a video
+
 video = cv.VideoCapture(video_file)
 assert video.isOpened(), 'Cannot read the given input, ' + video_file
 
-# Define the codec and create VideoWriter object
-fourcc = cv.VideoWriter_fourcc(*'XVID')  # Codec 설정 (여기서는 MPEG-4 코덱 사용)
+fourcc = cv.VideoWriter_fourcc(*'XVID')  
 
-# Run distortion correction
 show_rectify = True
 map1, map2 = None, None
 while True:
-    # Read an image from the video
     valid, img = video.read()
     if not valid:
         break
 
-    # Rectify geometric distortion (Alternative: `cv.undistort()`)
     info = "Original"
     if show_rectify:
         if map1 is None or map2 is None:
@@ -33,13 +28,10 @@ while True:
         info = "Rectified"
     cv.putText(img, info, (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
 
-    # Define VideoWriter object inside the loop
     out = cv.VideoWriter('output.mp4', fourcc, 20.0, (img.shape[1], img.shape[0]))
 
-    # Write the frame into the file
     out.write(img)
 
-    # Show the image and process the key event
     cv.imshow("Geometric Distortion Correction", img)
     key = cv.waitKey(10)
     if key == ord(' '):     # Space: Pause
@@ -49,7 +41,7 @@ while True:
     elif key == ord('\t'):  # Tab: Toggle the mode
         show_rectify = not show_rectify
 
-# Release everything if job is finished
+
 video.release()
 out.release()
 cv.destroyAllWindows()
